ad33d32ab0d9120350b09ae35c21dcd58b6a3f94
oktaysadoglu <oktaysadoglu@gmail.com>
2025-06-08 16:08:04 +0300

    exception handlers have been fixed

--- src/main/java/com/ing/credit/config/GlobalExceptionHandler.java
+++ src/main/java/com/ing/credit/config/GlobalExceptionHandler.java
@@ -3,11 +3,14 @@ package com.ing.credit.config;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -20,6 +23,34 @@ public class GlobalExceptionHandler {
         body.put("path", request.getRequestURI());
         body.put("error", ex.getClass().getSimpleName());
         body.put("message", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(body);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, Object>> handleRuntimeException(RuntimeException ex, HttpServletRequest request) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("path", request.getRequestURI());
+        body.put("error", ex.getClass().getSimpleName());
+        body.put("message", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
+    }
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<Map<String, Object>> handleRuntimeException(BindException ex, HttpServletRequest request) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("path", request.getRequestURI());
+        body.put("error", ex.getClass().getSimpleName());
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .toList();
+
+        body.put("message", errors);
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
     }
